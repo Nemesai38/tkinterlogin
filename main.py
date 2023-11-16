98579adce6ca074cd2a6ac9b7130ab2eb89e2f63
Nemesai38 <pappysouls@gmail.com>
2023-11-16 16:27:26 +0100

    Login py file needed to be run by main-py(got it!)

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from kivymd.app import MDApp
 from kivy.uix.screenmanager import Screen
 from kivymd.uix.button import MDFlatButton, MDRectangleFlatButton, MDIconButton, MDFloatingActionButton
+from subprocess import call
 
 
 class DemoApp(MDApp):
@@ -8,16 +9,22 @@ class DemoApp(MDApp):
         self.theme_cls.primary_palette = "Green"
         self.theme_cls.theme_style = "Dark"
         screen = Screen()
-        btn_flat = MDRectangleFlatButton(text='Hello World', pos_hint={'center_x': 0.5, 'center_y': 0.6})
-        icon_btn = MDIconButton(icon='android', pos_hint={'center_x': 0.5, 'center_y': 0.5})
+        btn_flat = MDRectangleFlatButton(text='Sign In Here', pos_hint={'center_x': 0.5, 'center_y': 0.6},
+                                         on_release=self.run_login)
+        icon_btn = MDIconButton(icon='android', pos_hint={'center_x': 0.5, 'center_y': 0.5}, on_release=self.run_login)
         float_btn = MDFloatingActionButton(icon='android', md_bg_color='orange',
-                                           pos_hint={'center_x': 0.5, 'center_y': 0.4})
-        flat_btn = MDFlatButton(text="Sign In", pos_hint={'center_x': 0.5, 'center_y': 0.3})
+                                           pos_hint={'center_x': 0.5, 'center_y': 0.4}, on_release=self.run_login)
+        flat_btn = MDFlatButton(text="No Sign In here", pos_hint={'center_x': 0.5, 'center_y': 0.3},
+                                on_release=self.run_login)
         screen.add_widget(icon_btn)
         screen.add_widget(btn_flat)
         screen.add_widget(float_btn)
         screen.add_widget(flat_btn)
         return screen
+
+    @staticmethod
+    def run_login(self):
+        call(["python", "login.py"])
 
 
 DemoApp().run()
